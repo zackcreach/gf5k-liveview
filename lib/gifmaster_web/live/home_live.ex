89083bfd5d6b91ec16323b8d06788254c179aee6
Zack Creach <zackcreach@gmail.com>
2024-12-23 17:27:25 -0500

    following simple s3 upload guide

--- lib/gifmaster_web/live/home_live.ex
+++ lib/gifmaster_web/live/home_live.ex
@@ -13,13 +13,13 @@ defmodule GifmasterWeb.HomeLive do
     |> assign(
       title: "Gifmaster 5000",
       description: "The best gifs you ever did see",
-      show_gif_upload_modal: true,
+      show_gif_upload_modal: false,
       get_gifs_form: to_form(%{"search" => ""}),
       gif_changeset: Gif.changeset(%Gif{}),
       gifs: AsyncResult.loading()
     )
     |> assign_async(:gifs, fn -> {:ok, %{gifs: Catalog.get_gifs()}} end)
-    |> allow_upload(:gif, accept: [".gif"], max_entries: 1)
+    |> allow_upload(:gif, accept: [".gif"], max_entries: 1, external: &presign_upload/2)
     |> ok()
   end
 
@@ -47,6 +47,29 @@ defmodule GifmasterWeb.HomeLive do
       <.form for={@gif_changeset}></.form>
     </.modal>
     """
+  end
+
+  defp presign_upload(entry, socket) do
+    uploads = socket.assigns.uploads
+    bucket = "gems.gifmaster5000.com"
+    key = entry.client_name
+
+    config = %{
+      region: "us-east-1",
+      access_key_id: System.fetch_env!("AWS_ACCESS_KEY_ID"),
+      secret_access_key: System.fetch_env!("AWS_SECRET_ACCESS_KEY")
+    }
+
+    {:ok, fields} =
+      SimpleS3Upload.sign_form_upload(config, bucket,
+        key: key,
+        content_type: entry.client_type,
+        max_file_size: uploads[entry.upload_config].max_file_size,
+        expires_in: :timer.hours(1)
+      )
+
+    meta = %{uploader: "S3", key: key, url: "http://#{bucket}.s3-#{config.region}.amazonaws.com", fields: fields}
+    {:ok, meta, socket}
   end
 
   def handle_event("search", %{"search" => search}, socket) do
